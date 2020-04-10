d4d2beff6cf73ccc0b99ad4c9c8468445aec307d
Jeovanny Giron <codixsal@gmail.com>
2020-04-10 08:02:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
       + "2. " + katzDeliLine.slice(1,2) + ", "
       + "3. " + katzDeliLine.slice(2,3)
    }
-  } else if {
+  } else if (katzDeliLine === 0) {
         return "The line is currently empty."
   }
 }
